f0969858a7de58b535556f560955bcbd4640dc40
killer <shianqi@imudges.com>
2016-10-21 19:10:47 +0800

    更新了蒙古文译中文当没有相关解释或者结果时的显示

--- view3/view3.js
+++ view3/view3.js
@@ -46,13 +46,15 @@ angular.module('myApp.view3', ['ngRoute'])
                 return;
             }
             $scope.isLoading = true;
+            $scope.results = [];
+            $scope.abouts = [];
+            $scope.resultsSize = 0;
+            $scope.aboutsSize = 0;
+            $scope.results2 = [];
+
             var data = {'judge':'传统蒙古文--汉文' , 'userInput':$scope.inputText};
             $http.post("http://dic.mglip.com/Default.aspx/GetArray",data)
                 .success(function (date) {
-                    $scope.resultsSize = 0;
-                    $scope.aboutsSize = 0;
-                    $scope.results = [];
-                    $scope.abouts = [];
                     date.d.forEach(function (e) {
                         $scope.results2.push(e);
                         if(e.mogInteCode==$scope.inputText||e.mogInteCode==$scope.inputText+' '){
@@ -71,6 +73,24 @@ angular.module('myApp.view3', ['ngRoute'])
                 });
             //var response = {"d":[{"__type":"Dictionary","id":0,"chinese":"吃饱 ","mogInteCode":" ","source":null,"character":null,"mogMekcode":null,"mogLatin":null,"mogLatinAlert":null,"mogInteAlert":null,"classify":null,"english":null,"newMog":"гэдэс гарах ","oldMogExplain":null,"newMogExplain":null,"mogIntelletualCode":null,"mogMongoliaCode":null,"middleEncode":null,"chineseCount":0},{"__type":"Dictionary","id":0,"chinese":"吃饱;饱足 ","mogInteCode":" ","source":null,"character":null,"mogMekcode":null,"mogLatin":null,"mogLatinAlert":null,"mogInteAlert":null,"classify":null,"english":null,"newMog":"цадах ","oldMogExplain":null,"newMogExplain":null,"mogIntelletualCode":null,"mogMongoliaCode":null,"middleEncode":null,"chineseCount":0},{"__type":"Dictionary","id":0,"chinese":"使之吃饱;让人吃饱 ","mogInteCode":" ","source":null,"character":null,"mogMekcode":null,"mogLatin":null,"mogLatinAlert":null,"mogInteAlert":null,"classify":null,"english":null,"newMog":"цатгах ","oldMogExplain":null,"newMogExplain":null,"mogIntelletualCode":null,"mogMongoliaCode":null,"middleEncode":null,"chineseCount":0}]};
         };
+
+        $scope.haveAbout = function () {
+            if($scope.aboutsSize==0){
+                return false;
+            }else{
+                return true;
+            }
+
+        };
+        $scope.haveResult = function () {
+            if($scope.resultsSize==0){
+                return false;
+            }else{
+                return true;
+            }
+
+        };
+
         $scope.haveResults = function(){
             if($scope.results.length==0&&$scope.abouts.length==0){
                 return false;
